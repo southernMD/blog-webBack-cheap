7c664dc41cd3496d54016cb9cb6690dee7213f48
southernMD <2483723241@qq.com>
2023-01-19 21:46:22 +0800

    fix2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,29 @@
-import axios from "@/api/axiosInit"
+import request from "axios";
+
+const axios = request.create({
+    baseURL:`http://${window.location.hostname}:3102`,
+    withCredentials:true
+}) 
+
+//请求拦截器，在发起请求前做一些事情
+axios.interceptors.request.use((config)=>{
+    //包含请求头headers
+    return config;
+})
+
+//相应拦截器
+axios.interceptors.response.use((response) =>{
+    console.log(response);
+    console.log(response.data);
+    let data = response.data;
+    return response.data;
+  },  (error)=>{
+    if(error.response.status == 403){
+        window.location.reload()
+    }
+    return Promise.reject(error);
+});
+
 
 export const Login = (qs:PostQs)=>{
     //返回的是promise对象
